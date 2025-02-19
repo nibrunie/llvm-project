873bbd9db286546e4dfacaa07c4329dc0ad30609
Nicolas Brunie <nibrunie@gmail.com>
2025-02-18 21:34:45 -0800

    [Zvbc32e/Zvkgs] introducing some changes required for fast track vector crypto extensions

--- clang/include/clang/Support/RISCVVIntrinsicUtils.h
+++ clang/include/clang/Support/RISCVVIntrinsicUtils.h
@@ -504,6 +504,8 @@ enum RVVRequire : uint32_t {
   RVV_REQ_Zvksed = 1 << 14,
   RVV_REQ_Zvksh = 1 << 15,
   RVV_REQ_Experimental = 1 << 16,
+  RVV_REQ_Zvbc32e = 1 << 17,
+  RVV_REQ_Zvkgs = 1 << 18,
 
   LLVM_MARK_AS_BITMASK_ENUM(RVV_REQ_Experimental)
 };
